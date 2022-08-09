ad2fbf9721ada8e5f91da66c2a46cdd56d8c3fa4
Jan <mail@jan-claar.de>
2022-08-09 16:56:01 +0200

    feat: adapt cityscapes Dataset for simplified use

---
+++ src/cityscapes_dataset.py
@@ -0,0 +1,72 @@
+import os
+from torchvision.datasets import VisionDataset
+from torchvision.datasets.cityscapes import Cityscapes
+from torchvision.datasets.utils import extract_archive
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+
+from PIL import Image
+
+
+class CustomCityscapesDataset(VisionDataset):
+	"""
+
+	"""
+
+	# Based on https://github.com/mcordts/cityscapesScripts
+	classes = Cityscapes.classes
+
+	def __init__(self, root_dir: str = 'data', mode: str = 'train', transform: Optional[Callable] = None,
+	             target_transform: Optional[Callable] = None,
+	             transforms: Optional[Callable] = None, ) -> None:
+
+		super(CustomCityscapesDataset, self).__init__(root_dir, transforms, transform, target_transform)
+
+		self.root_dir = root_dir
+		self.image_dir = os.path.join(root_dir, 'leftImg8bit', mode)
+		self.target_dir = os.path.join(root_dir, 'gtFine', mode)
+		self.images = []
+		self.targets = []
+
+		# Try to extract zips if unzipped files are not present
+		if not os.path.isdir(self.image_dir) or not os.path.isdir(self.target_dir):
+			image_dir_zip = os.path.join(self.root_dir, 'leftImg8bit_trainvaltest.zip')
+			target_dir_zip = os.path.join(self.root_dir, 'gtFine_trainvaltest.zip')
+
+			if os.path.isfile(image_dir_zip) and os.path.isfile(target_dir_zip):
+				extract_archive(from_path=image_dir_zip, to_path=self.root_dir)
+				extract_archive(from_path=target_dir_zip, to_path=self.root_dir)
+			else:
+				raise RuntimeError(
+					"Dataset not found or incomplete. Please make sure all required folders for the"
+					' specified "mode" are inside the "root" directory')
+
+		for city in os.listdir(self.image_dir):
+			img_dir = os.path.join(self.image_dir, city)
+			target_dir = os.path.join(self.target_dir, city)
+			for file_name in os.listdir(img_dir):
+				target_name = "{}_{}".format(
+					file_name.split("_leftImg8bit")[0], 'gtFine_labelIds.png'
+				)
+				self.images.append(os.path.join(img_dir, file_name))
+				self.targets.append(os.path.join(target_dir, target_name))
+
+	def __len__(self) -> int:
+		return len(self.images)
+
+	def __getitem__(self, index: int) -> Tuple[Any, Any]:
+		"""
+		:param index: The Index of the sample
+		:return: (image, target) where target is the pixel level segmentation (labelIds) of the image
+		"""
+		image = Image.open(self.images[index]).convert('RGB')
+		target = Image.open(self.targets[index])
+
+		if self.transforms is not None:
+			image, target = self.transforms(image, target)
+		else:
+			if self.transform is not None:
+				image = self.transform(image)
+			if self.target_transform is not None:
+				target = self.target_transform(target)
+
+		return image, target
