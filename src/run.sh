0e2209496bc8c6a8fa0446d06efaa1a91bf8b98c
Jan Claar <jan@server.com>
2022-10-25 10:46:23 +0000

    runs

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,3 @@
-python train.py -bs 5 -rn res34d_upConv_AlbuV1_s80 -enc resnet34d -lr 1e-4 -lrsp 100 -lrsf 0.1 -mf "../runs/Cityscapes/res34d_upConv_AlbuV1_lrsp_5_lrsf_0.5_bs_16_lr_0.0001/model_best.pth.tar" -red 80;
-python train.py -bs 5 -rn res34d_upConv_AlbuV1_s160 -enc resnet34d -lr 1e-4 -lrsp 100 -lrsf 0.1 -mf "../runs/Cityscapes/res34d_upConv_AlbuV1_lrsp_5_lrsf_0.5_bs_16_lr_0.0001/model_best.pth.tar" -red 160;
-python train.py -bs 5 -rn res34d_upConv_AlbuV1_s240 -enc resnet34d -lr 1e-4 -lrsp 100 -lrsf 0.1 -mf "../runs/Cityscapes/res34d_upConv_AlbuV1_lrsp_5_lrsf_0.5_bs_16_lr_0.0001/model_best.pth.tar" -red 240;
-python train.py -bs 5 -rn res34d_upConv_AlbuV1_s320 -enc resnet34d -lr 1e-4 -lrsp 100 -lrsf 0.1 -mf "../runs/Cityscapes/res34d_upConv_AlbuV1_lrsp_5_lrsf_0.5_bs_16_lr_0.0001/model_best.pth.tar";
+# python train.py -bs 10 -rn res34d_upConv_AlbuV1_s320 -enc resnet34d -lr 1e-4 -lrsp 100 -lrsf 0.1 -mf "../runs/Cityscapes/res34d_upConv_AlbuV1_lrsp_5_lrsf_0.5_bs_16_lr_0.0001/model_best.pth.tar";
+python train.py -bs 16 -rn res34d_upConv_AlbuV2 -enc resnet34d -lr 1e-4 -lrsp 10 -lrsf 0.5;
+python train.py -bs 32 -rn res34d_upConv_AlbuV2 -enc resnet34d -lr 1e-4 -lrsp 10 -lrsf 0.5;
