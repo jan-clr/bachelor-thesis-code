31012e91eaef9e59ef81c9c74b9cec3b957d21c1
Jan Claar <Jan@Uni>
2022-12-12 16:59:33 +0000

    feat: use hard labels for teacher masks

--- src/losses.py
+++ src/losses.py
@@ -2,14 +2,19 @@ import torch
 import torch.nn as nn
 
 
-def cross_entropy_cons_loss(pred_stu: torch.Tensor, pred_tch: torch.Tensor):
-	"""
-	Takes the softmax of the teacher prediction and returns the CrossEntropyLoss between the teacher and student predictions. Gradient only applied to student.
-	:param pred_stu: student prediction logits
-	:param pred_tch: teacher prediction logits
-	:return:
-	"""
-	loss_fn = nn.CrossEntropyLoss()
-	probs_tch = nn.functional.softmax(pred_tch, dim=1)
+def cross_entropy_cons_loss(pred_stu: torch.Tensor, pred_tch: torch.Tensor, hard_labels=True):
+        """
+        Takes the softmax of the teacher prediction and returns the CrossEntropyLoss between the teacher and student predictions. Gradient only applied to student.
+        :param pred_stu: student prediction logits
+        :param pred_tch: teacher prediction logits
+        :param hard_labels: use argmax on teacher predictions
+        :return:
+        """
+        loss_fn = nn.CrossEntropyLoss()
+        target = None
+        if hard_labels:
+            target = nn.functional.argmax(pred_tch, dim=1)
+        else:
+            target = nn.functional.softmax(pred_tch, dim=1)
 
-	return loss_fn(pred_stu, probs_tch)
+        return loss_fn(pred_stu, target)
