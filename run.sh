eab4fcb491864ac1a59c42bfb9c18b0f6db4b14a
Jan Claar <Jan@Uni>
2022-12-20 13:50:26 +0000

    feat: make CONTINUE flag setabel in terminal

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 # python train.py -bs 10 -rn res34d_upConv_AlbuV1_s320 -enc resnet34d -lr 1e-4 -lrsp 100 -lrsf 0.1 -mf "../runs/Cityscapes/res34d_upConv_AlbuV1_lrsp_5_lrsf_0.5_bs_16_lr_0.0001/model_best.pth.tar";
-# python train.py -bs 16 -rn res34d_nomt_aug2_100lbls -enc resnet34d -lr 1e-4 --no-lrs -lrsp 15 -lrsf 0.5 -lblr -100 --no-mt;
-python train.py -bs 16 -rn res34d_mt_aug2_lrson_100lbls -enc resnet34d -lr 1e-3 --lrs -lrsp 15 -lrsf 0.5 -lblr -100 -ulblr 100- --dropout 0.3 --mtdelay 10;
+# python train.py -bs 16 -rn res34d_nomt_aug2_20lbls -enc resnet34d -lr 1e-4 --lrs -lrsp 30 -lrsf 0.5 -lblr -20 --no-mt;
+python train.py -bs 16 -rn res34d_mt_lrson_100lbls_do_0.5_cow -enc resnet34d -lr 1e-3 --lrs -lrsp 5 -lrsf 0.1 -lblr -100 -ulblr 100- --dropout 0.5 --mtdelay 10;
+python train.py -bs 16 -rn res34d_mt_lrson_100lbls_do_0.5_cow -enc resnet34d -lr 1e-3 --lrs -lrsp 5 -lrsf 0.1 -lblr -100 -ulblr 100- --dropout 0.5 --mtdelay 0;
+python train.py -bs 14 -rn res34d_mt_lrson_100lbls_do_0.5_cow -enc resnet34d -lr 3e-5 --lrs -lrsp 5 -lrsf 0.1 -lblr -100 -ulblr 100- --dropout 0.5 --mtdelay 10;
+python train.py -bs 14 -rn res34d_mt_lrson_100lbls_do_0.5_cow -enc resnet34d -lr 3e-5 --lrs -lrsp 5 -lrsf 0.1 -lblr -100 -ulblr 100- --dropout 0.5 --mtdelay 0;
