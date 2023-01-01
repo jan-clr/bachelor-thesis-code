85133951247868b7ce57624d88d00ea244c583bb
Jan Claar <j.claar@liwetec.com>
2023-01-01 14:38:05 +0100

    feat: write detection

---
+++ src/detection.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+import torch
+from skimage import measure
+
+
+def detect_droplets(image):
+    image[image != 2] = 0
+    droplets = measure.label(image)
+    nr_droplets = np.unique(droplets) - 1
+    circles = []
+    for i in range(1, nr_droplets + 2):
+        pixel_locs = np.where(droplets == i)
+        center = np.mean(pixel_locs, dim=0)
+        distances = []
+        for loc in pixel_locs:
+            distances.append(np.linalg.norm(loc - center))
+        radius = np.mean(np.array(distances))
+        circles.append({'center': center, 'radius': radius})
+
+    return circles
